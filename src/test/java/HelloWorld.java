8f40847f1485779ee579516c8bbfe08e6b7e84e1
amarbabu-1 <amar_rupanagudi@yahoo.com>
2022-06-24 18:03:45 -0700

    Update HelloWorld.java
    updated master file.

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String []args){
         System.out.println("Hello");
         System.out.println("new subbranch line");
+        System.out.println("updated in Master file");
     }
 }
